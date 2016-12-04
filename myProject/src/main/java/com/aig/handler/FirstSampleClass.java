4bcf8a3b995ce207d2409025f924cf59dff27e12
F1 <F1@F1-PC>
2016-12-04 18:49:45 +0530

    1st commit

---
+++ myProject/src/main/java/com/aig/handler/FirstSampleClass.java
@@ -0,0 +1,18 @@
+package com.aig.handler;
+
+import java.util.logging.Logger;
+
+public class FirstSampleClass {
+
+	public static void main(String[] args) {
+		int i=1;
+		int j=2;
+		int z;
+		
+		z = i + j;
+		System.out.println(z);
+		
+
+	}
+
+}
